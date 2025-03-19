90295c9203ca6454775db15930532a7341a09b4f
Saifullahomar786 <73582075+Saifullahomar786@users.noreply.github.com>
2025-03-19 19:15:55 +0500

    Update and rename RebelDogeToken.sol to RandomToken.sol

--- RandomToken.sol
+++ RandomToken.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract RebelDogeToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit {
+contract RandomToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit {
     constructor(address initialOwner)
         ERC20("Rebel Doge Token", "RDT")
         Ownable(initialOwner)
